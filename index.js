d26531f98a85cea43c0fcdf96e6ce2c019e90999
Hannah Glazier <hrayglazier@aol.com>
2022-02-14 20:59:29 -0700

    end of day Monday

--- index.js
+++ index.js
@@ -6,9 +6,11 @@
 // const form = document.querySelector("form");
 // form.addEventListener("submit", submitForm);
 const container  = document.querySelector(".container-fluid") //BD -added for .append purposes
-const author = document.getElementById('author')
-const title = document.getElementById('title')
-const thumbnail = document.getElementById('img-thumbnail')
+let author = document.querySelector('.author')
+let title = document.querySelector('.title')
+let thumbnail = document.querySelector('.img-thumbnail')
+const bookCard = document.createElement('div')
+const bookBar = document.getElementById('book-bar')
 
 //Listeners
 
@@ -17,32 +19,40 @@ const thumbnail = document.getElementById('img-thumbnail')
 function fetchBooks(){
   return fetch(`https://www.googleapis.com/books/v1/volumes?q=SecretHistory`)
   .then(response => (response.json()))
+  // .then(console.log)
 }
  
 
 //Render Functions
 function renderAllBooks(bookObj){
-   console.log(bookObj.items)
- 
+  renderBookCard(bookObj.items)
 }
 
 
-function renderOneBook(bookObj){
-  for (const i = 0; i < 10; i++) {
-    author.innerText = (bookObj["items"][i]["volumeInfo"]["authors"]) || 'No Author Disclosed'
-    title.innerText  = (bookObj["items"][i]["volumeInfo"]["title"]) || 'No title Disclosed'
-    // try {
-    // thumbnail.src = bookObj["items"][i]["volumeInfo"]["imageLinks"]["thumbnail"] 
-    // }
-    // catch (err) {
-    //     thumbnail.src = 'https://upload.wikimedia.org/wikipedia/commons/thumb/a/a5/Georgia_404.svg/1125px-Georgia_404.svg.png'
-    // }
-}}
+function renderBookCard(bookObj){
+  // for (let i = 0; i < 10; i++) {
+  author.textContent = ` Author Name: ${(bookObj[0]["volumeInfo"]["authors"])}`
+  thumbnail.src = bookObj[0]["volumeInfo"]["imageLinks"]["thumbnail"]
+  
 
-
-function renderBookDetail(bookObj){
-
+  
 }
+// }
+
+
+function renderOneBook(bookObj){ //span bar
+  const span = document.createElement('span')
+  span.innerHTML = `<span>${bookObj.thumbnail}</span>`
+  span.addEventListener("click", function () {
+   renderBookCard(bookObj)
+  
+    });
+  
+  bookBar.appendChild(span, 'span')
+  
+}
+
+console.log(bookBar)
 function submitForm(){
 
 }
@@ -62,3 +72,25 @@ fetchBooks().then(bookObj => renderAllBooks(bookObj))
 //https://www.googleapis.com/books/v1/volumes?q=${searchInput}&key=AIzaSyD0p5CvqgLLo1tlpljQoUJly-4aTkXI_bE
 
 // `https://www.googleapis.com/books/v1/volumes?q=SecretHistory`
+
+
+
+  // for (const i = 0; i < 10; i++) {
+    // author.innerText = (bookObj[0]["volumeInfo"]["authors"])
+    // author.innerText = (bookObj["items"][0]["volumeInfo"]["authors"])
+    // (bookObj["items"][0]["volumeInfo"]["authors"]) 
+    // title.innerText  = (bookObj["items"][1]["volumeInfo"]["title"]) || 'No title Disclosed'
+    // try {
+    // thumbnail.src = bookObj["items"][i]["volumeInfo"]["imageLinks"]["thumbnail"] 
+    // }
+    // catch (err) {
+    //     thumbnail.src = 'https://upload.wikimedia.org/wikipedia/commons/thumb/a/a5/Georgia_404.svg/1125px-Georgia_404.svg.png'
+    // }
+// }
+
+// bookCard.id = bookObj.id
+// thumbnail.src = bookObj.imageLinks
+// author.textContent = bookObj.authors
+
+// bookCard.append(thumbnail, author)
+// container.appendChild(bookCard)
